34287495c7f1292a60131e3289f34d2089e70685
DonaldZheng <dzheng112@gmail.com>
2021-04-01 14:30:21 -0500

    added the seeded data the instructor gave to us

--- WebAPISample/Data/ApplicationContext.cs
+++ WebAPISample/Data/ApplicationContext.cs
@@ -15,7 +15,11 @@ namespace WebAPISample.Data
         {
             base.OnModelCreating(modelBuilder);
             // Seed data - needs migration
-           
+            modelBuilder.Entity<Movie>()
+             .HasData(
+                new Movie { MovieId = 1, Title = "The Departed", Genre = "Drama", DirectorName = "Martin Scorsese" },
+                new Movie { MovieId = 2, Title = "The Dark Knight", Genre = "Drama", DirectorName = "Christopher Nolan" },
+                new Movie { MovieId = 3, Title = "Inception", Genre = "Drama", DirectorName = "Christopher Nolan" });
         }
 
         public DbSet<Movie> Movies { get; set; }
